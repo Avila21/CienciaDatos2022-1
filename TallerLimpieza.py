8ead3ae2caebda480a642bb5661c4fa91a6e8fc5
Santiago_Avila21 <satavilag@correo.udistrital.edu.co>
2022-05-18 23:04:00 -0500

    Finish TallerLimpieza.py

--- TallerLimpieza.py
+++ TallerLimpieza.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+# --------------> Limpiar datos <---------------------
+
 ConectadosWifiTunja = pd.read_csv('WifiTunjaDataset.csv')
 
 df = pd.DataFrame({"nombre": ['Alfred', 'Batman', 'Catwoman'],
@@ -24,7 +26,58 @@ print("------")
 print(df.dropna(axis=1, how='any'))
 print("------")
 
-# Borra todos las columnas en los que haya al menos un NA
+# Llena todas las casillas con un NA con el valor dispuesto
 print(ConectadosWifiTunja.fillna(value=3).head(10))
 print("------")
 
+# Llena todas las casillas con un NA con el valor medio de las casillas con valor
+print(ConectadosWifiTunja.fillna(value=ConectadosWifiTunja.mean()).head(10))
+print("------")
+
+# Llena todas las casillas con un NA utilizando metodo especifico "bfill" toma el siguiente valor valido, limit nos indica el numero
+# de NaN consecutivos 
+print(ConectadosWifiTunja.fillna(method="bfill", limit=1).head(10))
+print("------")
+
+# --------------> Filtrado y Consulta de datos <---------------------
+
+# Retorna bool en todos los id en los que se cumple la condicion
+print(ConectadosWifiTunja['ANIO'] > 2021)
+print("------")
+
+# Retorna el objeto en todas las columnas donde se cumple la condicion
+print(ConectadosWifiTunja[ConectadosWifiTunja['ANIO'] > 2021])
+print("------")
+
+# Retorna la columna con todos los id que cumplen la condicion
+print(ConectadosWifiTunja['ANIO'][ConectadosWifiTunja['ANIO'] > 2021])
+print("------")
+
+# Retorna el objeto en todas las columnas donde se cumplen las condiciones dadas
+print(ConectadosWifiTunja[(ConectadosWifiTunja['ANIO'] > 2021) & (ConectadosWifiTunja['SECTOR'] < 5)])
+print("------")
+
+# --------------> Where, Mask, Isin, Query, Eval <---------------------
+
+# Reemplaza todos los valores en los que la condición es False
+print(ConectadosWifiTunja.where(ConectadosWifiTunja['ANIO'] > 2021))
+print("------")
+
+# Reemplaza todos los valores en los que la condición es True
+print(ConectadosWifiTunja.mask(ConectadosWifiTunja['ANIO'] > 2021))
+print("------")
+
+# La diferencia principal es que uno reemplaza los valores con la condicion en True y el otro con las condiciones en False
+# No solo se puede hacer el relleno con NaN tambien se puede hacer con escalares u otros Dataframe
+
+print(ConectadosWifiTunja.isin([5, 2021]))
+print("------")
+
+# La diferencia de pasarle como parametro una lista es que lo tomara directamente en todo el objeto y por valores
+# Mientras que si le pasamos un diccionario podemos hacer el filtro por columnas especificas
+
+print(ConectadosWifiTunja.eval('SUMALATLONG = LATITUD + LONGITUD '))
+print("------")
+
+# La diferencia principal entre eval y query es que eval, evalua un string con alguna expresion
+# Mientras que query realiza el filtro mediante una expresion booleana especificamente
